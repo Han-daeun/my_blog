9a128e0734b39bff66990a6a93d86abbbb7e861f
Han-daeun <eun71044@gmail.com>
2020-07-08 18:45:25 +0900

    옐로우핑거 상단 작업

---
+++ public/portfolio/yellow-finger/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Yellow finger</title>
+    <link rel="stylesheet" href="yellow-finger/index.css">
+    <link rel="stylesheet" href="yellow-finger/index.js">
+    <link rel="stylesheet" type="text/css"
+        href="//cdn.rawgit.com/innks/NanumSquareRound/master/nanumsquareround.min.css">
+</head>
+
+<body>
+    <div class="top-box-01 fixed">
+        <div class="top-bar con flex flex-ai-center">
+            <div class="logo flex flex-1-0-0">
+                <div class="logo-box relative">
+                    <a href="#" class="logo-border absolute">
+                        <img src="https://han-daeun.github.io/img1/pf/yellow-finger/menu_blank.png" alt="">
+                    </a>
+                    <a href="#" class="logo-simbol absolute"></a>
+                </div>
+                <a href="#" class="logo-text flex-ai-center">옐로우핑거</a>
+            </div>
+            <nav class="menu-bar">
+                <ul class="flex">
+                    <li><a href="#">WORK</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">LAB</a></li>
+                    <li><a href="#">CONTACT</a></li>
+                </ul>
+            </nav>
+            <div class="contact-btn flex-1-0-0 relative">
+                <a href="#" class="absolute"><span>프로젝트 문의</span></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="main-text-box con">
+        <p>논리와 감성의 균형<span>.</span></p>
+        <br>
+        옐로우핑거는 브랜드를 돋보이게 하는
+        <br class="type-03">
+        전문 디지털 에이전시 입니다.
+        <br>
+        우리는 프로젝트에 대한 이해와 공감을 바탕으로
+        <br>
+        디지털 커뮤니케이션의 리듬에 맞는 문맥 디자인과 개발에
+        <br class="type-03">
+        중점을 두며 창의적인 솔루션을 제공합니다.
+    </div>
+</body>
+
+</html>
