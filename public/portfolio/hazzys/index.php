9f80f9603448d75837b0768bf13428cab3da8e35
Han-daeun <eun71044@gmail.com>
2020-07-15 18:45:07 +0900

    헤지스 탑바 완성

--- public/portfolio/hazzys/index.php
+++ public/portfolio/hazzys/index.php
@@ -27,9 +27,9 @@
             </div>
         </div>
         <div class="top-bar-menu">
-            <nav class="menu-bar flex flex-jc-center flex-ai-center">
-                <ul class="flex">
-                    <li>
+            <nav class="menu-bar">
+                <ul class="flex flex-jc-center">
+                    <li class="flex flex-ai-center">
                         <a href="#">ARCHUVE</a>
                         <div>
                             <ul>
@@ -45,12 +45,206 @@
                             </ul>
                         </div>
                     </li>
-                    <li><a href="#">MEN</a></li>
-                    <li><a href="#">WOMEN</a></li>
-                    <li><a href="#">ACC</a></li>
-                    <li><a href="#">GOLF</a></li>
-                    <li><a href="#">BEAUTY</a></li>
-                    <li><a href="#">STORIES</a></li>
+                    <li class="flex flex-ai-center">
+                        <a href="#">MEN</a>
+                        <div>
+                            <ul>
+                                <div class="relative">
+                                    <div class="sub-menu-title"><a href="#">MEN</a></div>
+                                    <div class="sub-menu-list flex"  style="padding-top:30px;">
+                                        <a href="#" class="sub-title">MEN'S<br>HOME</a>
+                                        <div class="sub-menu-list-items flex">
+                                            <ul>
+                                                <li><a href="#">전체</a></li>
+                                                <li><a href="#">코트</a></li>
+                                                <li><a href="#">수트/정장</a></li>
+                                                <li><a href="#">가디건</a></li>
+                                            </ul>
+                                            <ul>
+                                                <li><a href="#">티셔츠</a></li>
+                                                <li><a href="#">셔츠</a></li>
+                                                <li><a href="#">자켓/베스트</a></li>
+                                                <li><a href="#">니트/스웨터</a></a></li>
+                                            </ul>
+                                            <ul>
+                                                <li><a href="#">점퍼</a></li>
+                                                <li><a href="#">팬츠</a></li>
+                                                <li><a href="#">언더/라운드웨어</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <span class="sub-menu-pic">
+                                        <img src="https://han-daeun.github.io/img1/pf/hazzys/d8560169f684467386e58547a6fb8471.jpg" alt="">
+                                    </span>
+                                </div>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="flex flex-ai-center">
+                        <a href="#">WOMEN</a>
+                        <div>
+                            <ul>
+                                <div class="relative">
+                                    <div class="sub-menu-title"><a href="#">WOMEN</a></div>
+                                    <div class="sub-menu-list flex"  style="padding-top:30px;">
+                                        <a href="#" class="sub-title">WOMEN'S<br>HOME</a>
+                                        <div class="sub-menu-list-items flex">
+                                            <ul>
+                                                <li><a href="#">전체</a></li>
+                                                <li><a href="#">자켓</a></li>
+                                                <li><a href="#">코트</a></li>
+                                                <li><a href="#">팬츠</a></li>
+                                                <li><a href="#">언더/라운지웨어</a></li>
+                                            </ul>
+                                            <ul>
+                                                <li><a href="#">가디건</a></li>
+                                                <li><a href="#">니트/스웨터</a></li>
+                                                <li><a href="#">티셔츠</a></a></li>
+                                                <li><a href="#">원피스</a></a></li>
+                                                <li><a href="#">여성비치웨어</a></li>
+                                            </ul>
+                                            <ul>
+                                                <li><a href="#">베스트</a></li>
+                                                <li><a href="#">점퍼</a></li>
+                                                <li><a href="#">셔츠</a></li>
+                                                <li><a href="#">스커트</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <span class="sub-menu-pic">
+                                        <img src="https://han-daeun.github.io/img1/pf/hazzys/e6cad8b04ba541efa74a12a6fe5c470d.jpg" alt="">
+                                    </span>
+                                </div>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="flex flex-ai-center">
+                        <a href="#">ACC</a>
+                        <div>
+                            <ul>
+                                <div class="relative">
+                                    <div class="sub-menu-title"><a href="#">ACC</a></div>
+                                    <div class="sub-menu-list flex"  style="padding-top:30px;">
+                                        <a href="#" class="sub-title">ACC'S<br>HOME</a>
+                                        <div class="sub-menu-list-items flex">
+                                            <ul>
+                                                <li><a href="#">전체</a></li>
+                                                <li><a href="#">스카프/머플러</a></li>
+                                                <li><a href="#">벨트</a></li>
+                                                <li><a href="#">키링/키홀더</a></li>
+                                                <li><a href="#">백팩</a></li>
+                                            </ul>
+                                            <ul>
+                                                <li><a href="#">여성가방</a></li>
+                                                <li><a href="#">남성가방</a></li>
+                                                <li><a href="#">양말</a></a></li>
+                                                <li><a href="#">모자</a></a></li>
+                                                <li><a href="#">여성 슈즈</a></li>
+                                            </ul>
+                                            <ul>
+                                                <li><a href="#">여성지갑</a></li>
+                                                <li><a href="#">남성지갑</a></li>
+                                                <li><a href="#">장갑</a></li>
+                                                <li><a href="#">기타ACC</a></li>
+                                                <li><a href="#">남성 슈즈</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <span class="sub-menu-pic">
+                                        <img src="https://han-daeun.github.io/img1/pf/hazzys/12cbd1d41fba4dd39b6cf01d95693df0.jpg" alt="">
+                                    </span>
+                                </div>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="flex flex-ai-center">
+                        <a href="#">GOLF</a>
+                        <div>
+                            <ul style="padding-bottom:62px;">
+                                <div class="relative">
+                                    <div class="sub-menu-title"><a href="#">GOLF</a></div>
+                                    <div class="sub-menu-list flex"  style="padding-top:30px;">
+                                        <a href="#" class="sub-title">GOLF'S<br>HOME</a>
+                                        <div class="sub-menu-list-items flex">
+                                            <ul>
+                                                <li><a href="#">전체</a></li>
+                                                <li><a href="#">남성하의</a></li>
+                                                <li><a href="#">여성하의</a></li>
+                                            </ul>
+                                            <ul>
+                                                <li><a href="#">남성아우터</a></li>
+                                                <li><a href="#">여성아우터</a></li>
+                                                <li><a href="#">골프용품</a></a></li>
+                                            </ul>
+                                            <ul>
+                                                <li><a href="#">남성상의</a></li>
+                                                <li><a href="#">여성상의</a></li>
+                                                <li><a href="#">골프가방</a></a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <span class="sub-menu-pic">
+                                        <img src="https://han-daeun.github.io/img1/pf/hazzys/14c3d10bb5144d7d9b8af247926c4a66.jpg" alt="">
+                                    </span>
+                                </div>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="flex flex-ai-center">
+                        <a href="#">BEAUTY</a>
+                        <div>
+                            <ul style="padding-bottom:110px;">
+                                <div class="relative">
+                                    <div class="sub-menu-title"><a href="#">BEAUTY</a></div>
+                                    <div class="sub-menu-list flex"  style="padding-top:30px;">
+                                        <a href="#" class="sub-title">BEAUTY'S<br>HOME</a>
+                                        <div class="sub-menu-list-items flex">
+                                            <ul>
+                                                <li><a href="#">전체</a></li>
+                                                <li><a href="#">남성케어</a></li>
+                                            </ul>
+                                            <ul>
+                                                <li><a href="#">스킨케어</a></li>
+                                            </ul>
+                                            <ul>
+                                                <li><a href="#">헤어/바디케어</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <span class="sub-menu-pic" style="right:327px;">
+                                        <img src="https://han-daeun.github.io/img1/pf/hazzys/b63f48ec619c41e88abe2c0a2bbbcd1d.jpg" alt="">
+                                    </span>
+                                    <span class="sub-menu-pic">
+                                        <img src="https://han-daeun.github.io/img1/pf/hazzys/435d4653857249d59fb7112fa3971d3c.jpg" alt="">
+                                    </span>
+                                </div>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="flex flex-ai-center">
+                        <a href="#">STORIES</a>
+                        <div>
+                            <ul style="padding-bottom:130px;">
+                                <div class="relative">
+                                    <div class="sub-menu-title"><a href="#">STORIES</a></div>
+                                    <div class="sub-menu-list flex"  style="padding-top:30px;">
+                                        <a href="#" class="sub-title">STORIES'S<br>HOME</a>
+                                        <div class="sub-menu-list-items flex">
+                                            <ul>
+                                                <li><a href="#" style="font-size:14px; font-weight:400;">CAMPAIGN</a></li>
+                                            </ul>
+                                            <ul>
+                                                <li><a href="#" style="font-size:14px; font-weight:400;">HAZZYS ICONIC ITEM</a></li>
+                                            </ul>
+                                            <ul>
+                                                <li><a href="#" style="font-size:14px; font-weight:400;">SPECIAL EDITION</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </ul>
+                        </div>
+                    </li>
                 </ul>
             </nav>
         </div>
