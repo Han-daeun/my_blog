2fbaf6457dc28a5fc85211cd0118043147751004
Han-daeun <eun71044@gmail.com>
2020-07-20 18:47:07 +0900

    라운드어라운드 상단바 & 레이아웃 1단계

---
+++ public/portfolio/rar/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <title>round a round</title>
+    <link rel="https://cdn.rawgit.com/mfd/f3d96ec7f0e8f034cc22ea73b3797b59/raw/856f1dbb8d807aabceb80b6d4f94b464df461b3e/gotham.css">
+    <link rel="stylesheet" href="/portfolio/rar/index.css">
+    <script src="/portfolio/rar/index.js"></script>
+    <link rel="stylesheet" type="text/css"
+        href="//cdn.rawgit.com/innks/NanumSquareRound/master/nanumsquareround.min.css">
+</head>
+<body>
+    <header class="top-bar flex-jc-between">
+        <div class="side-toggle-btn">
+            <span style="width:24px; height:1px; top:7px;"></span>
+            <span style="width:20px; height:1px; top:12px;"></span>
+            <span style="width:12px; height:1px; top:17px;"></span>
+        </div>
+        <div class="search-btn"></div>
+    </header>
+
+    <div class="container">
+        <div id="daily-life" class="section section-left">
+            <div class="text">
+                <div class="content">
+                    <div class="logo"></div>
+                    <div class="copy">일상 속 기분 좋은 휴식<br>라이프 코스메틱 <span style="font-weight:400; color:#000;">라운드어라운드</span>
+                    </div>
+                </div>
+            </div>
+            <div class="visual"></div>
+        </div>
+
+        <div id="story">
+            <div class="contents">
+                <p class="title">The Life Cosmetic<br>ROUND A'ROUND</p>
+                <p class="sub">라운드어라운드는 나를 둘러싼 일상에 기분 좋은 휴식을 선물하는<br>NO.1 라이프 코스메틱 브랜드입니다.
+                    <a href="#">Brans Story</a>
+                </p>
+            </div>
+        </div>
+
+        <div id="moments" class="section">
+            <div class="row">RARA Moments
+                <div class="img-box"></div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
