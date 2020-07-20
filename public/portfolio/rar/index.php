96dc2a9ccb183f99c712eedc6c7452dc1fd44905
Han-daeun <eun71044@gmail.com>
2020-07-21 00:34:25 +0900

    라운드어라운드 좌우 배치

--- public/portfolio/rar/index.php
+++ public/portfolio/rar/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <title>round a round</title>
+    <title>round a'round</title>
     <link rel="https://cdn.rawgit.com/mfd/f3d96ec7f0e8f034cc22ea73b3797b59/raw/856f1dbb8d807aabceb80b6d4f94b464df461b3e/gotham.css">
     <link rel="stylesheet" href="/portfolio/rar/index.css">
     <script src="/portfolio/rar/index.js"></script>
@@ -44,10 +44,155 @@
         </div>
 
         <div id="moments" class="section">
-            <div class="row">RARA Moments
-                <div class="img-box"></div>
+            <div class="row">
+                <h3>RARA Moments</h3>
+                <div class="img-box flex">
+                    <div class="box-01">
+                        <img src="https://han-daeun.github.io/img1/pf/rar/moment01.jpg" alt="">
+                    </div>
+                    <div class="box-02">
+                        <a href="#">
+                            <p>
+                                라운드어라운드와<br>함께하는 모든 행복의 순간
+                                <span>More</span>
+                            </p>
+                        </a>
+                        <p>
+                            <img src="https://han-daeun.github.io/img1/pf/rar/moment05.jpg" alt="">
+                        </p>
+                    </div>
+                    <div class="box-03">
+                        <div class="flex box-03-img">
+                            <p>
+                                <img src="https://han-daeun.github.io/img1/pf/rar/moment02.jpg" alt="">
+                            </p>
+                            <p>
+                                <img src="https://han-daeun.github.io/img1/pf/rar/moment03.jpg" alt="">
+                            </p>
+                        </div>
+                        <p>
+                            <img src="https://han-daeun.github.io/img1/pf/rar/moment04.jpg" alt="">
+                        </p>
+                        <div class="box-03-text">나와 나를 둘러싼 모든 일상이
+                            <br>더 아름다워집니다
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+
+        <div id="skincare" class="section section-right">
+            <div class="text">
+                <div class="content">
+                    <p style="font-size:40px; font-weight:300;margin-bottom:16px; margin-top:0;">Skin Care</p>
+                    <p style="font-weight:300; color:#555; margin:0;">바쁜 일상 속에서도 그 무엇보다 소중한
+                        <br>나의 피부를 위한 스킨케어를 제안합니다
+                        <a href="#">More</a>
+                    </p>
+                </div>
+                <div class="products type-right">
+                    <p class="title">Greentea<br>Skin Care</p>
+                    <p class="sub">피부를 위한<br>‘착한’ 스킨케어의 시작<br>그린티 수분 진정<br>스킨케어 라인
+                        <a href="#">More</a>
+                    </p>
+                    <div class="sub-img">
+                        <div>
+                            <a href="#">
+                                <img src="https://han-daeun.github.io/img1/pf/rar/51c45f341efb4b99b8c4365284ecbf1e.png" alt="">
+                                <span>그린티 약산성 클렌징폼</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="products type-left">
+                <p class="title">Basic<br>Skin Care</p>
+                    <p class="sub">꼭 필요한 것만 담아<br>매일이 더 편안한 피부<br>베이직 스킨케어 라인
+                        <a href="#">More</a>
+                    </p>
+                    <div class="sub-img">
+                        <div class="flex">
+                            <a href="#">
+                                <img src="https://han-daeun.github.io/img1/pf/rar/e042dc2f77276592e7f89fdc1e94f783.png" alt="">
+                                <span>리페어링 액티브 세럼</span>
+                            </a>
+                            <a href="#">
+                                <img src="https://han-daeun.github.io/img1/pf/rar/2ff6aa7c09d06fa881a27fe832dd6305.png" alt="">
+                                <span>컴포트 하이드레이팅 워터</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="visual"></div>
+        </div>
+
+        <div id="fragrance" class="section section-left">
+            <div class="text">
+                <div class="content">
+                    <p style="font-size:40px; font-weight:300;margin-bottom:16px; margin-top:0;">Fragrance</p>
+                    <p style="font-weight:300; color:#555; margin:0;">나와 나를 둘러싼 모든 시간과 공간을 더 가치있게
+                        <br>채워주는 라운드어라운드의 프래그런스
+                        <a href="#">More</a>
+                    </p>
+                </div>
+                <div class="products type-right">
+                    <p class="title">Home<br>Fragrance</p>
+                    <p class="sub">은은하게 오랫동안 지속되는<br>향기로 공간을 스타일링 하는<br>홈 프래그런스
+                        <a href="#">More</a>
+                    </p>
+                    <div class="sub-img">
+                        <div class="flex">
+                            <a href="#">
+                                <img src="https://han-daeun.github.io/img1/pf/rar/d7ddc258ee3515ca62bc42d23f470abf.png" alt="">
+                                <span>릴렉싱 필로우 미스트</span>
+                            </a>
+                            <a href="#">
+                                <img src="https://han-daeun.github.io/img1/pf/rar/2d7b1b9a73ad925a94cd5a419c8c3d2e.png" alt="">
+                                <span>선인장 디퓨저</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="products type-left">
+                <p class="title">Life<br>Fragrance</p>
+                    <p class="sub">남들과는 다르게 나의 일상을<br>특별하게 채우는 감성적인 향기
+                        <a href="#">More</a>
+                    </p>
+                    <div class="sub-img">
+                        <div class="flex">
+                            <a href="#">
+                                <img src="https://han-daeun.github.io/img1/pf/rar/12673b38437d638fa152214077861de8.png" alt="">
+                                <span>블룸 에디션</span>
+                            </a>
+                            <a href="#">
+                                <img src="https://han-daeun.github.io/img1/pf/rar/8952bcce8c3907e1259c866dd93c1997.png" alt="">
+                                <span>라이프 프래그런스</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="visual"></div>
+        </div>
+    </div>
+
+    <section class="sns-box">
+        <div>
+            <h3># ROUND A’ROUND</h3>
+            <p>
+                <a href="#">
+                    <span></span>
+                    roundaround_official
+                </a>
+            </p>
+            <div class="slider">
+                
+            </div>
+        </div>
+    </section>
+
+    <div class="footer">
+
     </div>
 
 </body>
