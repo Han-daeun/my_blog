0359ae55204d6f345edafd9ca536bf4e64cea2d0
Han-daeun <eun71044@gmail.com>
2020-07-28 18:50:08 +0900

    클로바 3페이지 레이아웃

--- public/portfolio/clova/index.php
+++ public/portfolio/clova/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,35 +9,158 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="/portfolio/clova/index.js"></script>
 </head>
+
 <body>
-    
-<div class="container">
-    <div class="swiper-slide" style="background-color:pink;">
-        <div class="section">
-            <div>
-                <div class="bg_main bg_main4" style="background:url(https://han-daeun.github.io/img1/pf/clova/bg_main_4.jpg);"></div>
-                <div class="bg_main bg_main3" style="background:url(https://han-daeun.github.io/img1/pf/clova/bg_main_3.jpg);"></div>
-                <div class="bg_main bg_main2" style="background:url(https://han-daeun.github.io/img1/pf/clova/bg_main_2.jpg);"></div>
-                <div class="bg_main bg_main1" style="background:url(https://han-daeun.github.io/img1/pf/clova/bg_main_1.jpg);"></div>
+
+    <div class="container">
+        <div class="swiper-slide page-01">
+            <div class="section">
+                <div class="slider">
+                    <div class="bg_main bg_main4 active"
+                        style="background:url(https://han-daeun.github.io/img1/pf/clova/bg_main_4.jpg) 50% 50%; background-size:cover;">
+                    </div>
+                    <div class="bg_main bg_main3"
+                        style="background:url(https://han-daeun.github.io/img1/pf/clova/bg_main_3.jpg) 50% 50%; background-size:cover;">
+                    </div>
+                    <div class="bg_main bg_main2"
+                        style="background:url(https://han-daeun.github.io/img1/pf/clova/bg_main_2.jpg) 50% 50%; background-size:cover;">
+                    </div>
+                    <div class="bg_main bg_main1"
+                        style="background:url(https://han-daeun.github.io/img1/pf/clova/bg_main_1.jpg) 50% 50%; background-size:cover;">
+                    </div>
+                </div>
+            </div>
+            <div class="main-text">
+                <div class="title-area">
+                    <h1></h1>
+                    <p></p>
+                </div>
             </div>
         </div>
+        <div class="swiper-slide page-02">
+            <div class="section flex-ai-center flex-jc-center">
+                <div class="select-friends">
+                    <strong>원하는 프렌즈를 선택해보세요.</strong>
+                    <em class="sub-title"></em>
+                    <div class="speaker-img flex flex-jc-center">
+                        <div class="speaker-item">
+                            <a href="#" class="speaker flex">
+                                <div>
+                                    <img src="https://han-daeun.github.io/img1/pf/clova/speaker_brown_main.png" alt="">
+                                </div>
+                                <div>
+                                    <img src="https://han-daeun.github.io/img1/pf/clova/speaker_sally_main.png" alt="">
+                                </div>
+                            </a>
+                            <div class="choice-btn">
+                                <a href="#">후디 커버 선택하기</a>
+                            </div>
+                        </div>
+                        <div class="speaker-item">
+                            <a href="#" class="speaker flex">
+                                <div>
+                                    <img src="https://han-daeun.github.io/img1/pf/clova/speaker_mini_brown_main.png"
+                                        alt="">
+                                </div>
+                                <div>
+                                    <img src="https://han-daeun.github.io/img1/pf/clova/speaker_mini_sally_main.png"
+                                        alt="">
+                                </div>
+                            </a>
+                            <div class="choice-btn">
+                                <a href="#">음성 답변 들어보기</a>
+                            </div>
+                        </div>
+                        <div class="speaker-item">
+                            <a href="#" class="speaker flex">
+                                <div>
+                                    <img src="https://han-daeun.github.io/img1/pf/clova/speaker_minions_kevin_main.png"
+                                        alt="">
+                                </div>
+                                <div>
+                                    <img src="https://han-daeun.github.io/img1/pf/clova/speaker_minions_bob_main.png"
+                                        alt="">
+                                </div>
+                            </a>
+                            <div class="choice-btn flex flex-jc-center">
+                                <a href="#" style="width:50%;">미니언즈와 대화하기</a>
+                                <a href="#" style="width:50%;">Face 스티커 선택하기</a>
+                            </div>
+                        </div>
+                        <div class="speaker-item">
+                            <a href="#" class="speaker">
+                                <div>
+                                    <img src="https://han-daeun.github.io/img1/pf/clova/speaker_doraemon_main.png"
+                                        alt="">
+                                </div>
+                            </a>
+                            <div class="choice-btn">
+                                <a href="#">도라에몽과 대화하기</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="swiper-slide page-03">
+            <div class=" section flex-ai-center flex-jc-center">
+                <div class="select-friends">
+                    <strong>원하는 명령어를 선택하고, 프렌즈 미니의 답변을 들어보세요.</strong>
+                    <em class="sub-title"></em>
+                    <div class="speaker-area flex flex-jc-center">
+                        <div class="speaker-area-img flex">
+                            <div>
+                                <img src="https://han-daeun.github.io/img1/pf/clova/speaker_mini_brown_on.png" alt="">
+                            </div>
+                            <div>
+                                <img src="https://han-daeun.github.io/img1/pf/clova/speaker_mini_sally_on.png" alt="">
+                            </div>
+                        </div>
+                        <div class="speach-area">
+                            <div>
+                                <a href="#">헤이 클로바, 오늘 강남구 날씨 어때?</a>
+                            </div>
+                            <div>
+                                <a href="#">헤이 클로바, 오늘 제주도 초미세먼지 지수 알려줘</a>
+                            </div>
+                            <div>
+                                <a href="#">헤이 클로바, 랩해줘</a>
+                            </div>
+                            <div>
+                                <a href="#">헤이 클로바, 올해 추석 연휴가 언제야?</a>
+                            </div>
+                            <div>
+                                <a href="#">헤이 클로바, 서울에서 강릉까지 얼마나 걸려?</a>
+                            </div>
+                            <div>
+                                <a href="#">헤이 클로바, 코스피 지수 알려줘</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="speach-btn">
+                        <a href="#">캐릭터</a>
+                    </div>
+                    <p class="notice">
+                    클로바 프렌즈 및 미니 전 제품에서 공통적으로 사용 가능한 명령어 입니다. 명령어에 대한 답변은 실제 클로바 프렌즈 미니의 답변을 예시로 구성되었으며,
+                    <br>
+                    5분 간격으로 업데이트 됩니다. 클로바 프렌즈 미니를 구매하시면, 위 명령어 예시 외에도 다양한 명령어를 사용할 수있습니다.
+                    <br>
+                    호출명은 [네이버 클로바 앱 > 디바이스 메뉴 > 호출명] 에서 수정할 수 있습니다.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="swiper-slide page-04">
+            <div class="section"></div>
+        </div>
+        <div class="swiper-slide page-05">
+            <div class="section"></div>
+        </div>
+        <div class="swiper-slide page-06">
+            <div class="section"></div>
+        </div>
     </div>
-    <div class="swiper-slide" style="background-color:gold;">
-
-    </div>
-    <div class="swiper-slide" style="background-color:skyblue;">
-
-    </div>
-    <div class="swiper-slide" style="background-color:lime;">
-
-    </div>
-    <div class="swiper-slide" style="background-color:orange;">
-
-    </div>
-    <div class="swiper-slide" style="background-color:gray;">
-
-    </div>
-</div>
 
 </body>
+
 </html>
