64a94248a9aa62a2d2ab3828f38bde1edeee5a92
Han-daeun <eun71044@gmail.com>
2020-07-29 18:45:37 +0900

    레이아웃 수정

--- public/portfolio/clova/index.php
+++ public/portfolio/clova/index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>클로바 온라인 쇼룸</title>
     <link rel="stylesheet" href="/portfolio/clova/index.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.9/fullpage.min.css"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="/portfolio/clova/index.js"></script>
 </head>
@@ -13,7 +14,7 @@
 <body>
 
     <div class="container">
-        <div class="swiper-slide page-01">
+        <div class="swiper-slide page-01" style="height:969px;">
             <div class="section">
                 <div class="slider">
                     <div class="bg_main bg_main4 active"
@@ -37,12 +38,13 @@
                 </div>
             </div>
         </div>
-        <div class="swiper-slide page-02">
+
+        <div class="swiper-slide page-02" style="height:969px;">
             <div class="section flex-ai-center flex-jc-center">
                 <div class="select-friends">
                     <strong>원하는 프렌즈를 선택해보세요.</strong>
                     <em class="sub-title"></em>
-                    <div class="speaker-img flex flex-jc-center">
+                    <div class="speaker-img flex">
                         <div class="speaker-item">
                             <a href="#" class="speaker flex">
                                 <div>
@@ -102,7 +104,8 @@
                 </div>
             </div>
         </div>
-        <div class="swiper-slide page-03">
+        <!--
+        <div class="swiper-slide page-03" style="height:969px;">
             <div class=" section purchase flex-ai-center flex-jc-center">
                 <div class="select-friends">
                     <strong>원하는 명령어를 선택하고, 프렌즈 미니의 답변을 들어보세요.</strong>
@@ -150,7 +153,7 @@
                 </div>
             </div>
         </div>
-        <div class="swiper-slide page-04">
+        <div class="swiper-slide page-04" style="height:969px;">
             <div class="section purchase flex-ai-center flex-jc-center">
                 <div class="select-friends">
                     <strong style="color:white;">원하는 명령어를 선택하고, 미니언즈의 답변을 들어보세요.</strong>
@@ -202,29 +205,149 @@
                 </div>
             </div>
         </div>
-        <div class="swiper-slide page-05">
-            <div class="section purchase">
+-->
+
+        <div class="swiper-slide page-03" style="height:969px;">
+            <div class="section purchase flex-ai-center flex-jc-center">
+                <div class="select-friends">
+                    <strong>원하는 명령어를 선택하고, 프렌즈 미니의 답변을 들어보세요.</strong>
+                    <em class="sub-title"></em>
+                    <div>
+                        <div class="speaker-area-img inline-block">
+                            <div class="inline-block">
+                                <img src="https://han-daeun.github.io/img1/pf/clova/speaker_mini_brown_on.png" alt="">
+                            </div>
+                            <div class="inline-block">
+                                <img src="https://han-daeun.github.io/img1/pf/clova/speaker_mini_sally_on.png" alt="">
+                            </div>
+                        </div>
+                        <div class="speach-area inline-block" style="vertical-align:top;">
+                            <div>
+                                <a href="#">헤이 클로바, 오늘 강남구 날씨 어때?</a>
+                            </div>
+                            <div>
+                                <a href="#">헤이 클로바, 오늘 제주도 초미세먼지 지수 알려줘</a>
+                            </div>
+                            <div>
+                                <a href="#">헤이 클로바, 랩해줘</a>
+                            </div>
+                            <div>
+                                <a href="#">헤이 클로바, 올해 추석 연휴가 언제야?</a>
+                            </div>
+                            <div>
+                                <a href="#">헤이 클로바, 서울에서 강릉까지 얼마나 걸려?</a>
+                            </div>
+                            <div>
+                                <a href="#">헤이 클로바, 코스피 지수 알려줘</a>
+                            </div>
+                        </div>
+                        <div class="speach-btn">
+                            <a href="#"></a>
+                        </div>
+                        <p class="notice">
+                            클로바 프렌즈 및 미니 전 제품에서 공통적으로 사용 가능한 명령어 입니다. 명령어에 대한 답변은 실제 클로바 프렌즈 미니의 답변을 예시로 구성되었으며,
+                            <br>
+                            5분 간격으로 업데이트 됩니다. 클로바 프렌즈 미니를 구매하시면, 위 명령어 예시 외에도 다양한 명령어를 사용할 수있습니다.
+                            <br>
+                            호출명은 [네이버 클로바 앱 > 디바이스 메뉴 > 호출명] 에서 수정할 수 있습니다.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="swiper-slide page-04" style="height:969px;">
+            <div class="section purchase flex-ai-center flex-jc-center">
+                <div class="select-friends">
+                    <strong style="color:white;">원하는 명령어를 선택하고, 미니언즈의 답변을 들어보세요.</strong>
+                    <em class="sub-title"></em>
+                    <div>
+                        <div class="speaker-area-img inline-block">
+                            <div>
+                                <img src="https://han-daeun.github.io/img1/pf/clova/speaker_minions_light_on.png"
+                                    alt="">
+                            </div>
+                        </div>
+                        <div class="speach-area inline-block" style="vertical-align:top;">
+                            <div>
+                                <a href="#">바나나 좋아해?</a>
+                            </div>
+                            <div>
+                                <a href="#">미니언즈 지금 뭐해?</a>
+                            </div>
+                            <div>
+                                <a href="#">오늘 기분 어때?</a>
+                            </div>
+                            <div>
+                                <a href="#">미니언 노래 불러줘</a>
+                            </div>
+                            <div>
+                                <a href="#">미니언 웃어봐</a>
+                            </div>
+                        </div>
+                        <div class="speach-btn">
+                            <a href="#"></a>
+                        </div>
+                        <p class="notice">
+                            미니언즈와 대화하기 기능은 클로바 프렌즈 미니언즈의 전용 기능입니다.
+                            <br>
+                            명령어에 대한 답변은 실제 클로바 프렌즈 미니언즈의 답변을 예시로 구성되었으며, 5분 간격으로 업데이트 됩니다.
+                            <br>
+                            클로바 프렌즈 미니언즈를 구매하시면, 위 명령어 예시 외에도 다양한 명령어를 사용할 수 있습니다.
+                            <span>
+                                <span>
+                                    <span class="clova"></span>
+                                    <span class="minions"></span>
+                                </span>
+                                Despicable Me, Minion Made and all related marks and characters are trademarks and
+                                <br>
+                                copyrights of Universal Studios. Licensed by Universal Studios. All Rights Reserved.
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="swiper-slide page-05" style="height:969px;">
+            <div class="section purchase flex flex-ai-center flex-jc-center">
                 <div class="select-friends">
                     <strong style="color:rgba(0, 101, 179, 0.8);">원하는 명령어를 선택하고, 도라에몽의 답변을 들어보세요.</strong>
                     <em class="sub-title"></em>
                     <div class="speaker-area">
                         <div class="speach-area">
-                            <a href="#" style="width:113px; height:48px; background-position: 0px -685px;"><span>안녕</span></a>
-                            <a href="#" style="width:163px; height:48px; background-position: -830px -555px;"><span>너는 누구야</span></a>
-                            <a href="#" style="width:162px; height:48px; background-position: -359px -346px;"><span>성별이 뭐야</span></a>
-                            <a href="#" style="width:111px; height:48px; background-position: -232px -685px;"><span>남자?</span></a>
-                            <a href="#" style="width:214px; height:48px; background-position: -878px -304px;"><span>좋아하는 사람 있어?</span></a>
-                            <a href="#" style="width:179px; height:48px; background-position: -851px -451px;"><span>나비가 누구야</span></a>
-                            <a href="#" style="width:187px; height:48px; background-position: -660px -451px;"><span>비밀도구 꺼내봐</span></a>
-                            <a href="#" style="width:149px; height:48px; background-position: -625px -633px;"><span>말놀이 하자</span></a>
-                            <a href="#" style="width:130px; height:48px; background-position: -525px -346px;"><span>다녀올게</span></a>
-                            <a href="#" style="width:109px; height:48px; background-position: -573px -685px;"><span>덥다</span></a>
-                            <a href="#" style="width:148px; height:48px; background-position: -930px -633px;"><span>생일축하해</span></a>
-                            <a href="#" style="width:162px; height:48px; background-position: -364px -246px;"><span>팥빵 먹을래?</span></a>
-                            <a href="#" style="width:166px; height:48px; background-position: -843px -503px;"><span>시험 빵점이야</span></a>
-                            <a href="#" style="width:149px; height:48px; background-position: -319px -633px;"><span>노래 불러줘</span></a>
-                            <a href="#" style="width:122px; height:48px; background-position: -1013px -503px;"><span>고마워</span></a>
-                            <a href="#" style="width:109px; height:48px; background-position: -686px -685px;"><span>잘자</span></a>
+                            <a href="#"
+                                style="width:113px; height:48px; background-position: 0px -685px;"><span>안녕</span></a>
+                            <a href="#" style="width:163px; height:48px; background-position: -830px -555px;"><span>너는
+                                    누구야</span></a>
+                            <a href="#" style="width:162px; height:48px; background-position: -359px -346px;"><span>성별이
+                                    뭐야</span></a>
+                            <a href="#"
+                                style="width:111px; height:48px; background-position: -232px -685px;"><span>남자?</span></a>
+                            <a href="#" style="width:214px; height:48px; background-position: -878px -304px;"><span>좋아하는
+                                    사람 있어?</span></a>
+                            <a href="#" style="width:179px; height:48px; background-position: -851px -451px;"><span>나비가
+                                    누구야</span></a>
+                            <a href="#" style="width:187px; height:48px; background-position: -660px -451px;"><span>비밀도구
+                                    꺼내봐</span></a>
+                            <a href="#" style="width:149px; height:48px; background-position: -625px -633px;"><span>말놀이
+                                    하자</span></a>
+                            <a href="#"
+                                style="width:130px; height:48px; background-position: -525px -346px;"><span>다녀올게</span></a>
+                            <a href="#"
+                                style="width:109px; height:48px; background-position: -573px -685px;"><span>덥다</span></a>
+                            <a href="#"
+                                style="width:148px; height:48px; background-position: -930px -633px;"><span>생일축하해</span></a>
+                            <a href="#" style="width:162px; height:48px; background-position: -364px -246px;"><span>팥빵
+                                    먹을래?</span></a>
+                            <a href="#" style="width:166px; height:48px; background-position: -843px -503px;"><span>시험
+                                    빵점이야</span></a>
+                            <a href="#" style="width:149px; height:48px; background-position: -319px -633px;"><span>노래
+                                    불러줘</span></a>
+                            <a href="#"
+                                style="width:122px; height:48px; background-position: -1013px -503px;"><span>고마워</span></a>
+                            <a href="#"
+                                style="width:109px; height:48px; background-position: -686px -685px;"><span>잘자</span></a>
                         </div>
                         <div class="speaker">
                             <img src="https://han-daeun.github.io/img1/pf/clova/speaker_doraemon_light_on.png" alt="">
@@ -233,16 +356,17 @@
                             <a href="#"></a>
                         </div>
                         <p class="notice">
-                        도라에몽과 대화하기 기능은 클로바 프렌즈 도라에몽의 전용 기능입니다. 명령어에 대한 답변은 실제 제품의 답변을 예시로 구성되었으며,
-                        <br>
-                        5분 간격으로 업데이트 됩니다. 이외 다양한 명령어 예시는 도라에몽 소개페이지 하단의 매뉴얼에서 확인하실 수 있습니다.
+                            도라에몽과 대화하기 기능은 클로바 프렌즈 도라에몽의 전용 기능입니다. 명령어에 대한 답변은 실제 제품의 답변을 예시로 구성되었으며,
+                            <br>
+                            5분 간격으로 업데이트 됩니다. 이외 다양한 명령어 예시는 도라에몽 소개페이지 하단의 매뉴얼에서 확인하실 수 있습니다.
                             <span></span>
                         </p>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="swiper-slide page-06">
+
+        <div class="swiper-slide page-06" style="height:969px;">
             <div class="section purchase flex flex-ai-center">
                 <div class="info-container">
                     <strong>클로바 프렌즈 구매처 안내</strong>
