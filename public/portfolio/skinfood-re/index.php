60aeff70b362c33f71637fc18aba75fa4a39f91c
Han-daeun <eun71044@gmail.com>
2020-08-11 23:01:18 +0900

    메인페이지 작업중

--- public/portfolio/skinfood-re/index.php
+++ public/portfolio/skinfood-re/index.php
@@ -5,7 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--제이쿼리적용-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!--슬릭슬라이더-->
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <title>스킨푸드</title>
+    <!--스타일적용-->
     <link rel="stylesheet" href="/portfolio/skinfood-re/index.css">
     <script src="/portfolio/skinfood-re/index.js"></script>
     <!--폰트적용-->
@@ -30,8 +34,62 @@
                 </div>
             </div>
         </div>
-        <div class="logo-bar"></div>
-        <div class="manu-bar"></div>
+        <div class="logo-bar">
+            <div class="logo flex flex-jc-center flex-ai-center">
+                <a href="#"><img src="https://han-daeun.github.io/img1/pf/skinfood/logo.png" alt=""></a>
+            </div>
+            <div class="sub-btn">
+                <a href="#"></a>
+                <a href="#"></a>
+            </div>
+        </div>
+        <nav class="menu-bar">
+            <ul class="con flex flex-jc-center flex-ai-center">
+                <li><a href="#">BRAND</a></li>
+                <li><a href="#">SKINCARE</a></li>
+                <li><a href="#">MAKE UP</a></li>
+                <li><a href="#">BODY·HAIR</a></li>
+                <li><a href="#">MEN</a></li>
+                <li><a href="#">CUMMUNITY</a></li>
+            </ul>
+        </nav>
     </div>
+    <div class="slide-box">
+        
+    </div>
+    <div class="shop-main con">
+        <div class="choice-box">
+            <div class="title-box">
+                <span class="title flex flex-jc-center">MD's CHOICE</span>
+                <span class="title-bar flex flex-jc-center"></span>
+                <span class="contents flex flex-jc-center">"망설이고있을 나를 위한 MD 강추상품!"</span>
+            </div>
+            <div class="clice-slide">
+                <div class="flex">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div class="slide-nav"></div>
+            </div>
+        </div>
+        <div class="best-item-box">
+            <div class="title-box">
+                <span class="title flex flex-jc-center">Category Best' Item</span>
+                <span class="title-bar flex flex-jc-center"></span>
+                <span class="contents flex flex-jc-center">"내 피부를 위한 특별한 선택"</span>
+            </div>
+        </div>
+        <div class="sns-slide"></div>
+    </div>
+    <div class="notice-page"></div>
+    <footer></footer>
 </body>
 </html>
