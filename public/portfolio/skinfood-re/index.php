1142ed4fe539bca9a76d4a2847ec8d48faf45586
Han-daeun <eun71044@gmail.com>
2020-08-11 18:45:26 +0900

    스킨푸드 최상단 루그인바 완성

---
+++ public/portfolio/skinfood-re/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--제이쿼리적용-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <title>스킨푸드</title>
+    <link rel="stylesheet" href="/portfolio/skinfood-re/index.css">
+    <script src="/portfolio/skinfood-re/index.js"></script>
+    <!--폰트적용-->
+    <link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/moonspam/NanumSquare/master/nanumsquare.css">
+</head>
+<body>
+
+    <div class="header">
+        <div class="top-bar">
+            <div class="top-bar-small flex flex-ai-center">
+                <div class="con flex">
+                    <div class="text-01">    
+                        <a href="#"><span>로그인</span></a>
+                        <a href="#"><span>회원가입</span></a>
+                        <a href="#"><span>장바구니</span></a>
+                    </div>
+                    <div class="text-02">
+                        <a href="#">커뮤니티</a>
+                        <a href="#">멤버십</a>
+                        <a href="#">이벤트</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="logo-bar"></div>
+        <div class="manu-bar"></div>
+    </div>
+</body>
+</html>
