ea2236eff12edf40833b0a6b4e8ac7f3e31bdccc
Han-daeun <eun71044@gmail.com>
2020-08-12 18:40:50 +0900

    공지박스, 푸터 완료

--- public/portfolio/skinfood-re/index.php
+++ public/portfolio/skinfood-re/index.php
@@ -82,11 +82,90 @@
             <span class="sns-contents flex flex-jc-center">#SKINFOOD</span>
         </div>
     </div>
-    <div class="notice-page con" style="width:100%; height:290px; background-color:#cdcdcd;">
-        <div class="notice-box"></div>
-        <div class="call-box"></div>
-        <div class="movie-box"></div>
+    <div class="notice-page con">
+        <div class="flex">    
+            <div class="notice-box">
+                <div class="flex flex-jc-between">
+                    <div class="notice-page-title">NOTICE</div>
+                    <a href="#" class="more-btn">
+                        <span></span>
+                        <span></span>
+                    </a>
+                </div>
+                <div class="notice-cont">
+                    <div><a href="#">8월 연휴 배송 일정 안내의 건</a></div>
+                    <div><a href="#">개인정보처리방침 문구 수정 및 공지</a></div>
+                    <div><a href="#">스킨푸드 개인정보 취급방침 변경 안내</a></div>
+                    <div><a href="#">스킨푸드 개인정보 취급방침 및 이용약관 변경 안내</a></div>
+                </div>
+            </div>
+            <div class="call-box">
+                <div class="notice-page-title">고객상담실</div>
+                <div class="open-time">
+                    <span class="flex">
+                        <p>평일</p>
+                        <p> AM&nbsp 09 : 00 &nbsp-&nbsp PM&nbsp 17 : 30</p>
+                    </span>
+                    <span class="flex">
+                        <p>점심시간</p>
+                        <p>AM&nbsp 11 : 30 &nbsp-&nbsp PM&nbsp 12 : 30</p>
+                    </span>
+                </div>
+                <span class="number">080-012-7878</span>
+            </div>
+            <div class="movie-box">
+                <div>
+                <iframe width="365" height="205" src="https://www.youtube.com/embed/ymcWOsCDoKg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
     </div>
-    <footer class="con" style="width:100%; height:400px; background-color:#e0e0e0;"></footer>
+    <footer>
+        <div class="con flex flex-jc-between">
+            <div class="ft-text">
+                <div class="btn-menu flex">
+                    <a href="#"><span>공지사항</span></a>
+                    <a href="#"><span>FAQ</span></a>
+                    <a href="#"><span>1:1 문의</span></a>
+                    <a href="#"><span>개인정보처리방침</span></a>
+                    <a href="#"><span>서비스 이용약관</span></a>
+                    <a href="#"><span>매장개설 문의</span></a>
+                    <a href="#"><span>제휴안내</span></a>
+                    <a href="#"><span>본사오시는길</span></a>
+                </div>
+                <div class="info">
+                    <span class="flex">
+                        <p>상호 : (주)스킨푸드</p>
+                        <p>대표이사 유근직</p>
+                        <p>Tel : 02-597-4191</p>
+                        <p>Fax : 02-597-4199</p>
+                    </span>
+                    <span class="flex">
+                        <p>주소 : 경기도 안성시 미양면 계륵리 267-4 스킨푸드 </p>
+                        <p>사업자등록번호 : 125-81-54503</p>
+                        <p>통신사업자번호 : 제2005-50호</p>
+                    </span>
+                    <span class="flex">
+                        <p>제휴문의 : webmaster@theskinfood.com</p>
+                        <p>호스팅사업자 : 영우디지탈</p>
+                    </span>
+                </div>
+                <div class="copy">
+                    <span>Copyright© 2015  SKINFOOD. ALL RIGHTS RESERVED.</span>
+                </div>
+            </div>
+            <div class="ft-sub-menu">
+                <div class="lang flex flex-jc-end">
+                    <a href="#">ENGLISH</a>
+                    <a href="#">CHINESE</a>
+                </div>
+                <div class="sns-btn flex">
+                    <a href="#" style="background-image:url(https://han-daeun.github.io/img1/pf/skinfood/f_logo_01.png);"></a>
+                    <a href="#" style="background-image:url(https://han-daeun.github.io/img1/pf/skinfood/Y_logo_01.png);"></a>
+                    <a href="#" style="background-image:url(https://han-daeun.github.io/img1/pf/skinfood/instagram-logo2.png);"></a>
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
