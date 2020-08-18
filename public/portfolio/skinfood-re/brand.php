96105872fc050fd9cb5a7b1058cc57ca74e810ac
Han-daeun <eun71044@gmail.com>
2020-08-18 18:48:07 +0900

    서브페이지 슬라이드 시작

--- public/portfolio/skinfood-re/brand.php
+++ public/portfolio/skinfood-re/brand.php
@@ -13,6 +13,79 @@ include "head.php"
     <div class="sub-main-banner">
         <img src="https://han-daeun.github.io/img1/pf/skinfood/subpage_banner.png" alt="">
     </div>
+    <div class="main-text flex-column flex-ai-center active-on-visible">
+        <div class="text-title">SKINFOOD</div>
+        <div class="text-bar"></div>
+        <div class="text-cont">
+            <span class="block">스킨푸드는 "Skin + Food"에서 유래한 "피부에 좋은 푸드"라는 네이밍의 의미에서</span>
+            <span class="block">'맛있는 푸드로 맍든 맛있는 화장품'이라는 모토와 함께 '푸드'라는 차별적이고 독특한 컨셉을 담아</span>
+            <span class="block">탄생한 최초의 푸드 코스메틱 브랜드 입니다.</span>
+        </div>
+    </div>
+    <div class="better-contents con">
+        <div class="better-01 flex" style="margin-top:73px">
+            <div class="better-img" style="background-image:url(https://han-daeun.github.io/img1/pf/skinfood/subimg01.png);"></div>
+            <div class="batter-contentns" style="margin-left:35px">
+                <div class="better-title">
+                    <div class="better-main-title">
+                        <span>01.</span>
+                        <span>Better Food</span>
+                    </div>
+                    <div class="better-sub-title">더 건강한 푸드를 위해서</div>
+                </div>
+                <div class="better-text">
+                    <div class="better-main-title-bar"></div>
+                    <span class="block">스킨푸드는 피부에 좋은 건강한 푸드를 찾기 위해 세계 곳곳의 푸드박람회는 꼭 챙겨가고,</span>
+                    <span class="block">피부에 종은 신선한 푸드를 구하기 위해 자연친화적인 방법으로 푸브를 재배하는 농부들을</span>
+                    <span class="block">만나러 전국을 누비기도 합니다. 당신이 좋아하는 푸드를 당신의 피부가 좋아할 수 있도록</span>
+                    <span class="block">더 좋은 푸드를 찾기 위한 스킨푸드의 여정은 오늘도 계속됩니다.</span>
+                </div>
+            </div>
+        </div>
+        <div class="better-02 flex flex-jc-end" style="margin-top:73px">
+            <div class="batter-contentns" style="margin-right:35px">
+                <div class="better-title">
+                    <div class="better-main-title">
+                        <span>02.</span>
+                        <span>Better Beauty</span>
+                    </div>
+                    <div class="better-sub-title">아름다운 피부를 위해서</div>
+                </div>
+                <div class="better-text">
+                    <div class="better-main-title-bar"></div>
+                    <span class="block">수많은 푸드를 직접 먹어보고, 발라보고, 공부하며 피부에 좋은 스킨푸드만의</span>
+                    <span class="block">독자적인 성분을 탄생시킵니다. 피부에 와닿는 푸드를 끊임없이 연구하는 스킨푸드!</span>
+                    <span class="block">아름다운 피부를 위해 꼭 필요한 푸드 뷰티의 힘을 전해드리겠습니다. </span>
+                </div>
+            </div>
+            <div class="better-img" style="background-image:url(https://han-daeun.github.io/img1/pf/skinfood/subimg02.png);"></div>
+        </div>
+        <div class="better-03 flex" style="margin-top:73px">
+            <div class="better-img" style="background-image:url(https://han-daeun.github.io/img1/pf/skinfood/subimg03.png);"></div>
+            <div class="batter-contentns" style="margin-left:35px">
+                <div class="better-title">
+                    <div class="better-main-title">
+                        <span>03.</span>
+                        <span>Better Healthy</span>
+                    </div>
+                    <div class="better-sub-title">당신의 생기넘치는 삶을 위해서</div>
+                </div>
+                <div class="better-text">
+                    <div class="better-main-title-bar"></div>
+                    <span class="block">건강한 피부로부터 전해지는 당당한 표정, 우아한 매너, 그리고 생기 넘치는 삶까지</span>
+                    <span class="block">아름답고 건강한 피부를 위한 스킨푸드만의 푸드스토리로</span>
+                    <span class="block">피부에 와닿는 Food & Beauty Lifestyle Brand가 되겠습니다.</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="food-slide">
+            <div class="slide-title flex-column flex-ai-center">
+                <span class="slide-title-01">FOOD STORY</span>
+                <span class="slide-title-02"></span>
+                <span class="slide-title-03">피부에 좋은 특별한 푸드이야기</span>
+            </div>
+    </div>
 
 
     <div class="sub-background">
--- public/portfolio/skinfood-re/index.js
+++ public/portfolio/skinfood-re/index.js
@@ -32,9 +32,107 @@ function subMenuBtn__init() {
 }
 
 
+/* 발견되면 액티브 실행 */
+function NumAni__start(selector) {
+    $(selector).each(function(index, node) {
+        var $el = $(node);
+        
+        var start = parseInt($el.attr('data-num-ani-start'));
+        var interval = parseInt($el.attr('data-num-ani-interval'));
+
+        $el.attr('data-num-ani-interval-current', interval);
+
+        $el.text(start);
+
+        NumAni__increaseNum($el);
+    });
+}
+
+function NumAni__increaseNum($el) {
+    var current = parseInt($el.text());
+    var end = parseInt($el.attr('data-num-ani-end'));
+    var stride = parseInt($el.attr('data-num-ani-stride'));
+    var interval = parseInt($el.attr('data-num-ani-interval-current'));
+    var slowPoint = parseFloat($el.attr('data-num-ani-slow-point'))
+
+    if (current < end) {
+        if ( current > end * slowPoint ) {
+            interval += parseInt($el.attr('data-num-ani-slow-add-interval'));
+            if ( interval > 100 ) {
+                interval = 100;
+            }
+            $el.attr('data-num-ani-interval-current', interval);
+        }
+
+        if ( current + stride > end ) {
+            $el.text(end);
+        }
+        else {
+            $el.text(current + stride);
+        }
+
+        setTimeout(function () {
+            NumAni__increaseNum($el);
+        }, interval);
+    }
+    else {
+        $el.addClass('num-action-done');
+    }
+}
+
+function ActiveOnVisible__init() {
+    $(window).resize(ActiveOnVisible__initOffset);
+    ActiveOnVisible__initOffset();
+
+    $(window).scroll(ActiveOnVisible__checkAndActive);
+    ActiveOnVisible__checkAndActive();
+}
+
+function ActiveOnVisible__initOffset() {
+    $('.active-on-visible').each(function(index, node) {
+        var $node = $(node);
+
+        var offsetTop = $node.offset().top;
+        $node.attr('data-active-on-visible-offsetTop', offsetTop);
+
+        if ( !$node.attr('data-active-on-visible-diff-y') ) {
+            $node.attr('data-active-on-visible-diff-y', '0');
+        }
+
+        if ( !$node.attr('data-active-on-visible-delay') ) {
+            $node.attr('data-active-on-visible-delay', '0');
+        }
+    });
+
+    ActiveOnVisible__checkAndActive();
+}
+
+function ActiveOnVisible__checkAndActive() { 
+    $('.active-on-visible:not(.actived)').each(function(index, node) {
+        var $node = $(node);
+
+        var offsetTop = $node.attr('data-active-on-visible-offsetTop') * 1;
+        var diffY = parseInt($node.attr('data-active-on-visible-diff-y'));
+        var delay = parseInt($node.attr('data-active-on-visible-delay'));
+
+        var callbackFuncName = $node.attr('data-active-on-visible-callback-func-name');
+
+        if ( $(window).scrollTop() + $(window).height() + diffY > offsetTop ) {
+            $node.addClass('active');
+
+            setTimeout(function() {
+                $node.addClass('active');
+                if ( window[callbackFuncName] ) {
+                    window[callbackFuncName]($node);
+                }
+            }, delay);
+        }
+    });
+}
 
 
 $(function () {
     subMenuBtn__init();
     choceSlide__init();
+    ActiveOnVisible__init();
 });
