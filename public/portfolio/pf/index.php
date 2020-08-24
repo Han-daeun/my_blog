5855c167e333f4500023b45870a925876b733064
Han-daeun <eun71044@gmail.com>
2020-08-24 18:32:53 +0900

    포폴페이지 php 생성

---
+++ public/portfolio/pf/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>portfolio</title>
+    <script scr="/portfolio/pf/index.js"></script>
+    <link rel="stylesheet" href="/portfolio/pf/index.css">
+</head>
+<body>
+    
+</body>
+</html>
