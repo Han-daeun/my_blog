c22dbf313f908f6c0ec081c65aa3fb5a01855fd6
Han-daeun <eun71044@gmail.com>
2020-08-27 02:05:39 +0900

    포폴페이지 메인 배치

--- public/portfolio/pf/index.php
+++ public/portfolio/pf/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,7 +9,43 @@
     <script scr="/portfolio/pf/index.js"></script>
     <link rel="stylesheet" href="/portfolio/pf/index.css">
 </head>
+
 <body>
-    
+    <section class="main-page">
+        <div class="main-box">
+            <div class="main-border"></div>
+            <div class="parallax">
+                <div class="parallax__layer parallax__layer__1">
+                    <img src="https://han-daeun.github.io/img1/pf/pf/main_04.png" />
+                </div>
+                <div class="parallax__layer parallax__layer__2">
+                    <img src="https://han-daeun.github.io/img1/pf/pf/main-03.png" />
+                </div>
+                <div class="parallax__layer parallax__layer__3">
+                    <img src="https://han-daeun.github.io/img1/pf/pf/main_moon.png" />
+                </div>
+                <div class="parallax__layer parallax__layer__4">
+                    <img src="https://han-daeun.github.io/img1/pf/pf/main_door.png" />
+                </div>
+                <div class="parallax__layer parallax__layer__5">
+                    <img src="https://han-daeun.github.io/img1/pf/pf/main-02.png" />
+                </div>
+                <div class="parallax__layer parallax__layer__6">
+                    <img src="https://han-daeun.github.io/img1/pf/pf/main-01.png" />
+                </div>
+                <div class="title-box">
+                    <div class="title-text">
+                        <div>
+                            <img src="https://han-daeun.github.io/img1/pf/pf/main-logo.svg" alt="">
+                        </div>
+                        <span>Web designer Han Daeun</span>
+                    </div>
+                    <div class="title-bar"></div>
+                </div>
+                <div class="parallax__cover"></div>
+            </div>
+        </div>
+    </section>
 </body>
+
 </html>
