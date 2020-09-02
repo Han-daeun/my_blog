12893ad07283c76ffdcc2679df5d38b6d651ef3c
Han-daeun <eun71044@gmail.com>
2020-09-03 02:29:41 +0900

    kpr 작업중

--- public/portfolio/kpr/index.js
+++ public/portfolio/kpr/index.js
@@ -3,16 +3,34 @@ function FullPage() {
 
     $(document).ready(function () {
         $('#fullpage').fullpage({
-            autoScrolling:false,
+            autoScrolling: false,
             navigation: true,
             scrollBar: true,
             navigationPosition: 'right',
             anchors: ['anchor1', 'anchor2', 'anchor3', 'anchor4', 'anchor5']
         });
     });
-    
-    }
 
-    $(function () {
-        FullPage();
+}
+
+/* 상단바 스크롤 */
+function topBar__init() {
+    var $header = $('.header');
+
+    $(window).scroll(function () {
+        var scrollTop = $(window).scrollTop();
+
+        if (scrollTop > 850) {
+            $header.addClass('active');
+        } else {
+            $header.removeClass('active');
+        }
     });
+}
+
+
+
+$(function () {
+    FullPage();
+    topBar__init();
+});
--- public/portfolio/kpr/index.php
+++ public/portfolio/kpr/index.php
@@ -57,7 +57,13 @@
                     <img src="https://han-daeun.github.io/img1/pf/kpr/about_pic.jpg" alt="">
                 </div>
             </div>
-            <div class="section-more-box absolute"></div>
+            <div class="section-more-box absolute">
+                <div class="more-box-bar"></div>
+                <a href="#" class="more-box-btn">
+                    <div></div>
+                    <div></div>
+                </a>
+            </div>
         </div>
 
         <div class="section page-03 section-box">
@@ -67,13 +73,82 @@
                     <div></div>
                 </div>
                 <div class="sec-title">KPR은 새로운 미디어 환경에 최적화된<br>
-전략적이고 통합적인 <span>커뮤니케이션 솔루션</span>과<br>
-아이디어와 기술이 결합한<br>
-<span>창의적인 컨텐츠를 제공</span>합니다</div>
-                <div></div>
+                    전략적이고 통합적인 <span>커뮤니케이션 솔루션</span>과<br>
+                    아이디어와 기술이 결합한<br>
+                    <span>창의적인 컨텐츠를 제공</span>합니다</div>
+                <div class="page-03-round-box">
+                    <div>
+                        <div class="text">Public Relations</div>
+                        <div class="icon">
+                            <img src="https://han-daeun.github.io/img1/pf/kpr/impact_icon.png" alt="">
+                        </div>
+                        <div class="text">Digital & Technology</div>
+                        <div class="icon">
+                            <img src="https://han-daeun.github.io/img1/pf/kpr/impact_icon.png" alt="">
+                        </div>
+                        <div class="text">Creative Contents</div>
+                        <div class="icon">
+                            <img src="https://han-daeun.github.io/img1/pf/kpr/impact_icon.png" alt="">
+                        </div>
+                        <div class="text">Consulting</div>
+                        <div class="icon">
+                            <img src="https://han-daeun.github.io/img1/pf/kpr/impact_icon.png" alt="">
+                        </div>
+                        <div class="text">IMC & Collaboraion</div>
+                        <div class="icon">
+                            <img src="https://han-daeun.github.io/img1/pf/kpr/impact_icon.png" alt="">
+                        </div>
+                        <div class="text">Marketing</div>
+                        <div class="icon">
+                            <img src="https://han-daeun.github.io/img1/pf/kpr/impact_icon.png" alt="">
+                        </div>
+                        <div class="text">Promotion</div>
+                        <div class="icon">
+                            <img src="https://han-daeun.github.io/img1/pf/kpr/impact_icon.png" alt="">
+                        </div>
+                        <div class="text">Creative Ideas</div>
+                        <div class="icon">
+                            <img src="https://han-daeun.github.io/img1/pf/kpr/impact_icon.png" alt="">
+                        </div>
+                        <div class="">Solutions</div>
+                    </div>
+                </div>
+                <div class="page-03-round-text">
+                    <div>
+                        <div>
+                        오랜 경험으로 축적된 Public Relations 전문역량을
+                        <br class="br_05">
+                        바탕으로
+                        <br class="br_04">
+                        디지털 테크놀로지, 크리에이티브 콘텐츠,
+                        <br>
+                        <br class="br_03">
+                        <br class="br_05">
+                        IMC 콜라보레이션, 마케팅,
+                        <br class="br_04">
+                        프로모션 영역 등
+                        <br class="br_05">
+                        커뮤니케이션 전부문에 걸쳐
+                        <span>Insightful한 전략 컨설팅</span>
+                        으로
+                        <br class="br_05">
+                        <br class="br_04">
+                        고객의
+                        <br class="br_03">
+                        구매 여정에서 가장
+                        <br>
+                        <span>Impactful한 고객 경험을 제공</span>
+                        하는
+                        <br class="br_04">
+                        <br class="br_05">
+                        커뮤니케이션 솔루션 그룹 입니다.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="pr_box">
                 <div></div>
             </div>
-            <div class="section-more-box absolute"></div>
         </div>
 
         <div class="section page-04 section-box">
@@ -83,7 +158,7 @@
                     <div></div>
                 </div>
                 <div class="sec-title">KPR은 지난 30년 간<br>
-<span>다양한 분야의 클라이언트와 함께 성장</span>해 왔습니다.</div>
+                    <span>다양한 분야의 클라이언트와 함께 성장</span>해 왔습니다.</div>
                 <div></div>
                 <div></div>
             </div>
@@ -100,7 +175,13 @@
                 <div></div>
                 <div></div>
             </div>
-            <div class="section-more-box absolute"></div>
+            <div class="section-more-box absolute">
+                <div class="more-box-bar"></div>
+                <a href="#" class="more-box-btn">
+                    <div></div>
+                    <div></div>
+                </a>
+            </div>
         </div>
 
         <div class="section page-06 section-box" style="background-color:black;">
@@ -110,7 +191,7 @@
                     <div></div>
                 </div>
                 <div class="sec-title"><span>통합 커뮤니케이션 솔루션 서비스 기업, KPR과 지금 만나보세요<br>
-여러분의 경험과 가치를 높여드리겠습니다</span></div>
+                        여러분의 경험과 가치를 높여드리겠습니다</span></div>
                 <div></div>
                 <div></div>
             </div>
