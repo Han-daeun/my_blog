f7a1b0c5098e4311783e215190a14cd261b3b81d
Han-daeun <eun71044@gmail.com>
2020-09-14 18:38:29 +0900

    오설록 메인 코딩중

---
+++ public/portfolio/osulloc-re/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>오설록</title>
+    <!--제이쿼리적용-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!--슬릭슬라이더-->
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <!--스와이프슬라이더-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/css/swiper.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/js/swiper.min.js"></script>
+    <title>스킨푸드</title>
+    <!--스타일적용-->
+    <link rel="stylesheet" href="/portfolio/osulloc-re/index.css">
+    <link rel="stylesheet" href="/portfolio/osulloc-re/font.css">
+    <script src="/portfolio/osulloc-re/index.js"></script>
+    <!--폰트적용-->
+    <link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/moonspam/NanumSquare/master/nanumsquare.css">
+</head>
+<body>
+    
+    <div class="main-page">
+        <nav class="main-menu flex flex-column flex-ai-center">
+            <div class="logo-box">
+                <img src="https://han-daeun.github.io/img1/pf/osulloc/main_logo.png" alt="">
+            </div>
+            <ul class="flex flex-jc-center flex-jc-between">
+                <li><a href="#">About OSULLOC</a></li>
+                <li><a href="#">Tea Life</a></li>
+                <li><a href="#">Shop</a></li>
+                <li><a href="#">Jeju Tea Museum</a></li>
+                <li><a href="#">Event</a></li>
+            </ul>
+        </nav>
+    </div>
+
+</body>
+</html>
