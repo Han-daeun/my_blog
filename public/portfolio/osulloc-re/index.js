218d9fcc4c4a3dfdcadbff6ca8758c8c9f1fbfc6
Han-daeun <eun71044@gmail.com>
2020-09-15 02:07:44 +0900

    오설록 메인 코딩중

---
+++ public/portfolio/osulloc-re/index.js
@@ -0,0 +1,19 @@
+function mainSlide__init() {
+    new Swiper( '.swiper-container', {
+
+        effect : 'fade', // 페이드 효과 사용
+        speed: 3000,
+        loop : true, // 무한 반복
+        pagination : { // 페이징 설정
+            el : '.swiper-pagination',
+            clickable : true, // 페이징을 클릭하면 해당 영역으로 이동, 필요시 지정해 줘야 기능 작동
+        },
+        autoplay: {
+            delay: 5000, 
+        },
+    });
+}
+
+$(function () {
+    mainSlide__init();
+});
--- public/portfolio/osulloc-re/index.php
+++ public/portfolio/osulloc-re/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -21,22 +22,98 @@
     <!--폰트적용-->
     <link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/moonspam/NanumSquare/master/nanumsquare.css">
 </head>
+
 <body>
-    
-    <div class="main-page">
+
+    <section class="main-page">
         <nav class="main-menu flex flex-column flex-ai-center">
             <div class="logo-box">
                 <img src="https://han-daeun.github.io/img1/pf/osulloc/main_logo.png" alt="">
             </div>
             <ul class="flex flex-jc-center flex-jc-between">
-                <li><a href="#">About OSULLOC</a></li>
-                <li><a href="#">Tea Life</a></li>
-                <li><a href="#">Shop</a></li>
-                <li><a href="#">Jeju Tea Museum</a></li>
-                <li><a href="#">Event</a></li>
+                <li>
+                    <a href="#">About OSULLOC</a>
+                    <ul>
+                        <li><a href="#">Brand Story</a></li>
+                        <li><a href="#">Since 1979</a></li>
+                        <li><a href="#">오설록 차밭 이야기</a></li>
+                        <li><a href="#">숫자로 보는 오설록</a></li>
+                        <li><a href="#">티하우스 메뉴</a></li>
+                        <li><a href="#">매거진</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Tea Life</a>
+                    <ul>
+                        <li><a href="#">상품 구매후기</a></li>
+                        <li><a href="#">선물하기</a></li>
+                        <li><a href="#">다다일상(정기구독)</a></li>
+                        <li><a href="#">맞춤추천</a></li>
+                        <li><a href="#">MD's PICK</a></li>
+                        <li><a href="#">매장 방문기</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Shop</a>
+                    <ul>
+                        <li><a href="#">Tea Shop</a></li>
+                        <li><a href="#">Tea ware Shop</a></li>
+                        <li><a href="#">Gift Shop</a></li>
+                        <li><a href="#">Weekly Best</a></li>
+                        <li><a href="#">뭉치면 할인 UP</a></li>
+                        <li><a href="#">오설록 라운지</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Jeju Tea Museum</a>
+                    <ul>
+                        <li><a href="#">티뮤지엄 소개</a></li>
+                        <li><a href="#">티스톤 예약</a></li>
+                        <li><a href="#">제주 오설록 맵</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Event</a>
+                    <ul>
+                        <li><a href="#">이벤트</a></li>
+                        <li><a href="#">온라인 쇼핑혜택</a></li>
+                        <li><a href="#">인스타그램</a></li>
+                        <li><a href="#">About Tea</a></li>
+                    </ul>
+                </li>
             </ul>
         </nav>
-    </div>
+        <div class="main-bg-slide">
+            <div class="swiper-container">
+                <div class="slider swiper-wrapper">
+                    <div class="bg_main bg_main4 swiper-slide"
+                        style="background-image:url(https://han-daeun.github.io/img1/pf/osulloc/main_bg_01.png); background-size:cover;">
+                        <div class="main-text-box" style="width:860px;">
+                            <p>차와 제주가 선사하는<br>삶의 아름다움</p>
+                        </div>
+                    </div>
+                    <div class="bg_main bg_main4 swiper-slide"
+                        style="background-image:url(https://han-daeun.github.io/img1/pf/osulloc/main_bg_02.png); background-size:cover;">
+                        <div class="main-text-box" style="width:1031px;">
+                            <p>천혜의 자연과<br>사람의 정성으로 만든 명차</p>
+                        </div>
+                    </div>
+                    <div class="bg_main bg_main4 swiper-slide"
+                        style="background-image:url(https://han-daeun.github.io/img1/pf/osulloc/main_bg_03.png); background-size:cover;">
+                        <div class="main-text-box" style="width:1039px;">
+                            <p>제주의 자연으로 빚은 발효<br>오설록</p>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="main-bg-box">
+            <div class="col"></div>
+            <div class="row"></div>
+        </div>
+    </section>
 
 </body>
+
 </html>
