a2a249494b2439054d5f8314a42e5f89e95966cc
Han-daeun <eun71044@gmail.com>
2020-09-16 02:41:28 +0900

    오설록 푸터 코딩

--- public/portfolio/osulloc-re/index.php
+++ public/portfolio/osulloc-re/index.php
@@ -114,6 +114,93 @@
         </div>
     </section>
 
+    <div class="md-box">
+        <div class="md-title-box"></div>
+        <div class="md-slide"></div>
+    </div>
+
+    <div class="osulloc-info">
+
+    </div>
+
+    <div class="weekly-best-box">
+
+    </div>
+
+    <div class="shop-box">
+
+    </div>
+
+    <div class="banner-box">
+
+    </div>
+
+    <div class="sns-box">
+
+    </div>
+
+
+    <footer>
+        <div class="ft-logo-box flex flex-jc-center">
+            <div class="logo-img"></div>
+        </div>
+        <div class="ft-cont flex flex-jc-center">
+            <div class="flex">
+                <div class="ft-left">
+                    <div class="left-cont-01 flex">
+                        <a href="#">회사소개</a>
+                        <a href="#">공지사항</a>
+                        <a href="#">서비스 이용약관</a>
+                        <a href="#">개인정보 처리방침</a>
+                        <a href="#">영상정보 처리방침</a>
+                        <a href="#">뷰티포인트</a>
+                        <a href="#">사이트맵</a>
+                        <a href="#">공고</a>
+                    </div>
+                    <div class="left-cont-02">
+                        <div>(주) 오설록</div>
+                        <div>
+                            <span>대표이사: 서혁재</span>
+                            <span>주소: 서울특별시 용산구 한강대로 100. 14층(한강로2가)</span>
+                            <span>사업자등록번호: 390-87-01499</span>    
+                        </div>
+                        <div>
+                            <span>통신판매업신고번호: 2019-서울용산-117호</span>    
+                            <span>호스팅제공자: (주)오설록</span>
+                        </div>
+                    </div>
+                    <div class="left-cont-03">
+                        <span>(주) 오설록은 오설록 브랜드를 제외한 입점 브랜드에 대해서는 통신판매둘개자 이며 통신판매의 장사자가 아닙니다.
+                        </span>
+                        <span>따라서 입점판매자가 등록한 상품정보 및 거래에 대해 책임을 지지 않습니다.
+                        </span>
+                    </div>
+                </div>
+                <div class="ft-right">
+                    <div class="tell-box flex">
+                        <div>
+                            <span>고객상담센터 · 주문/배송문의</span>
+                            <span>080 - 805 - 5555</span>
+                            <span>평일 09:30 - 17:00 (점심 11:30 - 13:00)</span>
+                        </div>
+                        <div>
+                            <span>특판 · 대량구매 문의</span>
+                            <span>help@osulloc.com</span>
+                            <span>카카오톡ID : osullocmall</span>
+                            <span>평일 09:30 - 17:00 (점심 11:30 - 13:00)</span>
+                        </div>
+                    </div>
+                    <div class="icon-box flex">
+                        <div><a href="#" style="background-image:url(https://han-daeun.github.io/img1/pf/osulloc/ft-img01_w.png);"></a></div>
+                        <div><a href="#" style="background-image:url(https://han-daeun.github.io/img1/pf/osulloc/ft-img02_w.png);"></a></div>
+                        <div><a href="#" style="background-image:url(https://han-daeun.github.io/img1/pf/osulloc/ft-img03_w.png);"></a></div>
+                        <div><a href="#" style="background-image:url(https://han-daeun.github.io/img1/pf/osulloc/ft-img04_w.png);"></a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <span>(주) 이니시스 구매안전서비스 : 고객님의 안전거래를 위해 현금 거래에 대해 이니페이 에스크로서비스를 이용하실 수 있습니다.</span>
+    </footer>
 </body>
 
 </html>
