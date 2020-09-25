273de71ee51ef68e581e6df580f916325c77b04b
Han-daeun <eun71044@gmail.com>
2020-09-25 18:44:15 +0900

    체크박스 동기화

--- public/portfolio/mf/join-02.js
+++ public/portfolio/mf/join-02.js
@@ -1,13 +1,54 @@
-function checkBox__init() {
-    $("#checkbox1").click(function(){
-        if($("#checkbox1").prop("checked")){
-            $("input[name=chk]").prop("checked",true);
-        }else{
-            $("input[name=chk]").prop("checked",false);
-        }
-    });
+// function checkBox__init() {
+//     $("#checkbox1").click(function(){
+//         if($("#checkbox1").prop("checked")){
+//             $("input[name=checkOne]").prop("checked",true);
+//         }else{
+//             $("input[name=checkOne]").prop("checked",false);
+//         }
+//     });
+// }
+
+// $(function () {
+//     checkBox__init();
+// });
+
+// 모두 => 동기화
+function allCheckFunc( obj ) {
+    $("[name=checkOne]").prop("checked", $(obj).prop("checked") );
 }
 
-$(function () {
-    checkBox__init();
+// 체크박스 체크시 전체선택 체크 여부 
+function oneCheckFunc( obj )
+{
+    var allObj = $("[name=checkAll]");
+    var objName = $(obj).attr("name");
+
+    if( $(obj).prop("checked") )
+    {
+        checkBoxLength = $("[name="+ objName +"]").length;
+        checkedLength = $("[name="+ objName +"]:checked").length;
+        
+        // console.log("checkBoxLength : " + checkBoxLength + ", checkedLength : " + checkedLength);
+        // 확인용
+
+        if( checkBoxLength == checkedLength ) {
+            allObj.prop("checked", true);
+        } else {
+            allObj.prop("checked", false);
+        }
+    }
+    else
+    {
+        allObj.prop("checked", false);
+    }
+}
+
+$(function(){
+    $("[name=checkAll]").change(function(){
+        allCheckFunc( this );
+    });
+    
+    $("[name=checkOne]").change(function(){
+        oneCheckFunc($(this));
+    });
 });
--- public/portfolio/mf/join-02.php
+++ public/portfolio/mf/join-02.php
@@ -24,23 +24,23 @@ include "head.php"
     </div>
     <div class="check-menu-box">
         <div class="checkbox-container">
-            <input type="checkbox" id="checkbox1"/>
+            <input type="checkbox" id="checkbox1" name="checkAll"/>
             <label for="checkbox1">약관에 모두 동의</label>
         </div>
         <div class="checkbox-container">
-            <input type="checkbox" id="checkbox2" name="chk"/>
+            <input type="checkbox" id="checkbox2" name="checkOne"/>
             <label for="checkbox2">(필수) 만 14세 이상입니다</label>
         </div>
         <div class="checkbox-container">
-            <input type="checkbox" id="checkbox3" name="chk"/>
+            <input type="checkbox" id="checkbox3" name="checkOne"/>
             <label for="checkbox3">(필수) 서비스 이용약관에 동의</label>
         </div>
         <div class="checkbox-container">
-            <input type="checkbox" id="checkbox4" name="chk"/>
+            <input type="checkbox" id="checkbox4" name="checkOne"/>
             <label for="checkbox4">(필수) 개인정보 수집이용에 동의</label>
         </div>
         <div class="checkbox-container">
-            <input type="checkbox" id="checkbox5" name="chk"/>
+            <input type="checkbox" id="checkbox5" name="checkOne"/>
             <label for="checkbox5">(선택) 광고 수신 및 마케팅 이용에 동의</label>
         </div>
     </div>
