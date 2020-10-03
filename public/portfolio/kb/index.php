9f29010bfa2c80708e6b544eff8940efdd11a8f6
Han-daeun <eun71044@gmail.com>
2020-10-04 00:08:39 +0900

    카카오뱅크 코딩중

--- public/portfolio/kb/index.php
+++ public/portfolio/kb/index.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>카카오뱅크</title>
+    <link rel='stylesheet' href='//cdn.jsdelivr.net/font-kopub/1.0/kopubdotum.css'>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <!--스와이프 슬라이드-->
+    <!--스와이프 슬라이더-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/css/swiper.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/js/swiper.min.js"></script>
     <link rel="stylesheet" href="/portfolio/kb/index.css">
@@ -173,20 +174,261 @@
         </div>
     </header>
 
-    <div class="banner-box">
-        <div class="banner-img con">
-            <div class="banner-cont">
-                <h3>같지만 다른 은행<br>
-                    카카오뱅크</h3>
-                <p>일상에서 더 쉽게, 더 자주 만나는 은행<br>
-                    카카오뱅크가 한번 더 편리해졌습니다.</p>
-                <ul class="icon-box">
-                    <li><a href="#"></a></li>
-                    <li><a href="#"></a></li>
+    <div class="section-01">
+        <div class="banner-box">
+            <div class="banner-img con">
+                <div class="banner-cont">
+                    <h3>같지만 다른 은행<br>
+                        카카오뱅크</h3>
+                    <p>일상에서 더 쉽게, 더 자주 만나는 은행<br>
+                        카카오뱅크가 한번 더 편리해졌습니다.</p>
+                    <ul class="icon-box flex">
+                        <li>
+                            <a href="#"><span></span>Google Play</a>
+                        </li>
+                        <li>
+                            <a href="#"><span></span>App Store</a>
+                        </li>
+                </div>
             </div>
         </div>
     </div>
+
+    <div class="section section-02">
+        <div class="section-02-image-box">
+            <img src="https://han-daeun.github.io/img1/pf/kb/main-withdrawal.png" alt="">
+        </div>
+        <div class="bn-cont-text">
+            <h3>모바일로 더 손쉬운<br>
+                계좌개설, 간편한 이체</h3>
+            <p>공인인증서, OTP 없이 계좌 개설이 간편합니다.<br>
+                여러 건의 이체도 몇 번의 터치로 손쉽게 보낼 수 있습니다.</p>
+        </div>
+        <div class="bn-cont-btn">
+            <a href="#">
+                <span>카카오뱅크 입출금통장</span>
+                <!--
+                <img src="https://han-daeun.github.io/img1/pf/kb/home_arr.png" alt="">
+-->
+            </a>
+        </div>
     </div>
+
+    <div class="border-bar"></div>
+
+    <div class="section section-03">
+        <div class="section-03-image-box">
+            <img src="https://han-daeun.github.io/img1/pf/kb/main-savings.png" alt="" class="active-on-visible">
+        </div>
+        <div class="bn-cont-text">
+            <h3>우대조건 없이<br>
+                합리적인 예금과 적금</h3>
+            <p>기대했던 금리와 다르게 복잡한 우대조건에 실망한 적이 있었나요?<br>
+                카카오뱅크의 예금, 적금은 숨겨진 우대조건 없이 누구에게나 편리하고 합리적입니다.</p>
+        </div>
+        <div class="bn-cont-btn flex">
+            <a href="#">
+                <span>정기예금</span>
+                <!--
+                <img src="https://han-daeun.github.io/img1/pf/kb/home_arr.png" alt="">
+-->
+            </a>
+            <a href="#">
+                <span>자유적금</span>
+                <!--
+                <img src="https://han-daeun.github.io/img1/pf/kb/home_arr.png" alt="">
+-->
+            </a>
+        </div>
+    </div>
+
+    <div class="border-bar"></div>
+
+    <div class="section section-04">
+        <div class="section-04-image-box">
+            <img src="https://han-daeun.github.io/img1/pf/kb/main-moim.png" alt="">
+        </div>
+        <div class="bn-cont-text">
+            <h3>함께 쓰고 같이 보는<br>
+                모임통장</h3>
+            <p>카카오톡 친구를 바로 모임통장으로 초대하고<br>
+                친구들과 함께 잔액과 입출금 현황을 확인할 수 있습니다.<br>
+                재미있는 메시지 카드로 회비 입금 요청도 해보세요.</p>
+        </div>
+        <div class="bn-cont-btn">
+            <a href="#">
+                <span>모임통장</span>
+                <!--
+                <img src="https://han-daeun.github.io/img1/pf/kb/home_arr.png" alt="">
+-->
+            </a>
+        </div>
+    </div>
+
+    <div class="border-bar"></div>
+
+    <div class="section section-05">
+        <div class="section-05-image-box">
+            <img src="https://han-daeun.github.io/img1/pf/kb/main-26weeks.png" alt="">
+        </div>
+        <div class="bn-cont-text">
+            <h3>함께 도전해서<br>
+                재미있는 26주적금</h3>
+            <p>26주동안 변화하는 재미에 빠져있는 사이<br>
+                어느덧 만기 달성 경험을 맛보게 됩니다.</p>
+        </div>
+        <div class="bn-cont-btn">
+            <a href="#">
+                <span>26주적금</span>
+                <!--
+                <img src="https://han-daeun.github.io/img1/pf/kb/home_arr.png" alt="">
+-->
+            </a>
+        </div>
+    </div>
+
+    <div class="border-bar"></div>
+
+    <div class="section section-06">
+        <div class="section-06-image-box">
+            <img src="https://han-daeun.github.io/img1/pf/kb/main-foreignRemittance.png" alt="" class="active-on-visible">
+        </div>
+        <div class="bn-cont-text">
+            <h3>해외계좌송금과<br>
+                WU빠른해외송금을<br>
+                더 쉽고, 저렴하게</h3>
+            <p>해외계좌송금이 가능한 22개국을 포함하여<br>
+                전세계 200여개국으로<br>
+                WU빠른해외송금이 가능합니다.</p>
+        </div>
+        <div class="bn-cont-btn">
+            <a href="#">
+                <span>해외송금</span>
+                <!--
+                <img src="https://han-daeun.github.io/img1/pf/kb/home_arr.png" alt="">
+-->
+            </a>
+        </div>
+    </div>
+
+    <div class="section section-07">
+        <div class="background-sky">
+            <div class="sky-blie">
+                
+            </div>
+        </div>
+        <div class="cont-text">
+            <h3>복잡한 서류 제출을 간편하게, 내가 원하는 시간에</h3>
+            <p>이사 날짜가 주말이나 공휴일이어도 전월세보증금 대출을 신청할 수 있습니다.</p>
+        </div>
+        <div class="bn-cont-btn flex">
+            <a href="#">
+                <span>마이너스 통장대출</span>
+            </a>
+            <a href="#">
+                <span>신용대출</span>
+            </a>
+            <a href="#">
+                <span>개인사업자 대출</span>
+            </a>
+            <a href="#">
+                <span>전월세보증금 대출</span>
+            </a>
+        </div>
+        <div class="cont-img">
+            <img src="https://han-daeun.github.io/img1/pf/kb/main-loan.png" alt="">
+        </div>
+    </div>
+
+    <div class="section section-08">
+        <div class="cont-text">
+            <h3>프렌즈 체크카드,<br>
+                내가 고르는 선택의 즐거움</h3>
+            <p>프렌즈 디자인부터 멋스러운 블랙 컬러 카드까지 원하는 디자인과 기능을 선택할 수 있습니다.</p>
+        </div>
+        <div class="bn-cont-btn">
+            <a href="#">
+                <span>카카오뱅크 프렌즈 체크카드</span>
+            </a>
+        </div>
+        <div class="kakao-slide">
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide"
+                        style="background-image: url(https://han-daeun.github.io/img1/pf/kb/card-3.png);"></div>
+                    <div class="swiper-slide"
+                        style="background-image: url(https://han-daeun.github.io/img1/pf/kb/card-4.png);"></div>
+                    <div class="swiper-slide"
+                        style="background-image: url(https://han-daeun.github.io/img1/pf/kb/card-5.png);"></div>
+                    <div class="swiper-slide"
+                        style="background-image: url(https://han-daeun.github.io/img1/pf/kb/card-1.png);"></div>
+                    <div class="swiper-slide"
+                        style="background-image: url(https://han-daeun.github.io/img1/pf/kb/card-2.png);"></div>
+                </div>
+            </div>
+            <div class="side-btn">
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section section-09">
+        <div class="cont-text">
+            <h3>IT 기술로 카카오뱅크를<br>
+                만들어 갑니다.</h3>
+        </div>
+        <span class="cont-bar"></span>
+        <span class="cont-bar-bg"></span>
+        <div class="cont-img">
+            <img src="https://han-daeun.github.io/img1/pf/kb/home_it.png" alt="">
+            <ul class="flex">
+                <li>
+                    <strong>간결하고, 유려하게</strong>
+                    <p>작은 모바일 화면에 적합하도록<br>
+                        복잡함을 덜어낸<br>
+                        유려하고 친화적인 UX</p>
+                </li>
+                <li>
+                    <strong>간편한 인증, 철저한 보안</strong>
+                    <p>지문, 비밀번호로 간편한 인증<br>
+                        IT 기술의 강력한 보안 검증과<br>
+                        데이터 암호화</p>
+                </li>
+                <li></li>
+                <li>
+                    <strong>Mobile First, One App</strong>
+                    <p>모바일에서 뱅킹이 더 쉽고 편리하게<br>
+                        여러 앱을 설치할 필요 없이<br>
+                        하나의 앱으로</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <footer>
+        <div class="footer-inner">
+            <div class="footer-info flex">
+                <a href="#">모바일뱅킹 서비스 이용약관</a>
+                <a href="#">전자금융거래 기본약관</a>
+                <a href="#" class="footer-bold">위치기반 서비스 이용약관</a>
+                <a href="#" class="footer-bold">개인정보처리방침</a>
+                <a href="#" class="footer-bold">전자민원접수</a>
+                <a href="#">보호금융상품등록부</a>
+                <a href="#">상품공시실</a>
+                <a href="#">경영공시</a>
+                <a href="#">공지사항</a>
+            </div>
+            <div class="footer-copy flex">
+                <span style="font-weight:bold;">대표전화 1599-3333</span>
+                <span style="padding: 0 19px 0 6px; font-weight: 200; color: #888;">(해외 +82-2-6420-3333)</span>
+                <span style="font-size: smaller; color: #888;">Copyright © KakaoBank Corp. All rights reserved.</span>
+            </div>
+            <div class="footer-bottom flex">
+
+            </div>
+        </div>
+    </footer>
 </body>
 
 </html>
