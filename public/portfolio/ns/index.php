458c9457ae65a12bc0b27b5996637afb9f356ab5
Han-daeun <eun71044@gmail.com>
2020-10-05 18:44:40 +0900

    ns홈쇼핑 코딩중

---
+++ public/portfolio/ns/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NS홈쇼핑</title>
+    <!--제이쿼리-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!--스와이프 슬라이더-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/css/swiper.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/js/swiper.min.js"></script>
+    <link rel="stylesheet" href="/portfolio/ns/index.css">
+    <script src="/portfolio/ns/index.js"></script>
+</head>
+
+<body>
+
+    <header class="top-bar-wrap">
+        <div class="top-bar con">
+            <div class="logo-left absolute"></div>
+            <div class="logo-right absolute"></div>
+            <div class="menu-box">
+                <nav>
+                    <ul class="flex">
+                        <li>
+                            <a href="#"><span>회사소개</span></a>
+                            <ul>
+                                <li><a href="#"><span>소개</span></a></li>
+                                <li><a href="#"><span>연혁</span></a></li>
+                                <li><a href="#"><span>주요사업</span></a></li>
+                                <li><a href="#"><span>오시는길</span></a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#"><span>지속가능경영</span></a>
+                            <ul>
+                                <li><a href="#"><span>고객중심경영</span></a></li>
+                                <li><a href="#"><span>상생경영</span></a></li>
+                                <li><a href="#"><span>윤리경영</span></a></li>
+                                <li><a href="#"><span>준법경영</span></a></li>
+                                <li><a href="#"><span>CCM인증</span></a></li>
+                                <li><a href="#"><span>UNGC</span></a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#"><span>입점안내</span></a>
+                            <ul>
+                                <li><a href="#"><span>입점절차</span></a></li>
+                                <li><a href="#"><span>상품선정위원회</span></a></li>
+                                <li><a href="#"><span>입점FAQ</span></a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#"><span>IR</span></a>
+                            <ul>
+                                <li><a href="#"><span>재무정보</span></a></li>
+                                <li><a href="#"><span>이사회구성</span></a></li>
+                                <li><a href="#"><span>전자공고</span></a></li>
+                                <li><a href="#"><span>IR정보</span></a></li>
+                                <li><a href="#"><span>IR (Eng)</span></a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#"><span>고객소통</span></a>
+                            <ul>
+                                <li><a href="#"><span>보도자료</span></a></li>
+                                <li><a href="#"><span>시청자위원회</span></a></li>
+                                <li><a href="#"><span>시청자의견</span></a></li>
+                                <li><a href="#"><span>Social NS</span></a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#"><span>인재채용</span></a>
+                            <ul>
+                                <li><a href="#"><span>인재상</span></a></li>
+                                <li><a href="#"><span>인재육성</span></a></li>
+                                <li><a href="#"><span>직무소개</span></a></li>
+                                <li><a href="#"><span>채용절차</span></a></li>
+                                <li><a href="#"><span>입사지원</span></a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <div class="nav-bg"></div>
+    </header>
+
+</body>
+
+</html>
